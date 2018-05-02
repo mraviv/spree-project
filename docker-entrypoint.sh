654210da9d2e9e2fde21ae24c779e4d966b58fa0
mraviv <33684523+mraviv@users.noreply.github.com>
2018-05-02 11:24:27 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This is entrypoint for docker image of spree sandbox on docker cloud
 
-#RDS_HOSTNAME=projectdb.crxcp6lem4bm.us-east-1.rds.amazonaws.com RDS_DB_NAME=spreeDB RDS_USERNAME=spree RDS_PASSWORD=dbpassword RAILS_ENV=development bundle exec rails s -p 3000 -b '0.0.0.0'
 RAILS_ENV=development bundle exec rails s -p 3000 -b '0.0.0.0'
